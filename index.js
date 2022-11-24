299352d38c583447e9b3c5a0aed0c5a1b8656ef1
shashankkumarP <nawalerason@gmail.com>
2022-11-24 12:12:05 +0530

    last_try

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const cors = require('cors');
 const router = require("./Router/Router");
 const connection = require("./db");
 const app=express();
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors());
 
 app.use(express.json())
 app.use("/",router)
